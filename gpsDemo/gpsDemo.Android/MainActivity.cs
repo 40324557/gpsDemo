8f285b6f328e23614ee870fc1275d29f79b0a44a
40324557 <40324557@users.noreply.github.com>
2019-01-31 13:13:10 +0000

    Initial commit

---
+++ gpsDemo/gpsDemo.Android/MainActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Plugin.Permissions;
+using Plugin.CurrentActivity;
+
+namespace gpsDemo.Droid
+{
+    [Activity(Label = "gpsDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            CrossCurrentActivity.Current.Activity = this;
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            LoadApplication(new App());
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ gpsDemo/gpsDemo/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Plugin.Geolocator;
+
+namespace gpsDemo
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            
+        }
+
+
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var locatorObj = CrossGeolocator.Current;
+            var position = await locatorObj.GetPositionAsync(TimeSpan.FromSeconds(10));
+            var lat = position.Latitude;
+            var lon = position.Longitude;
+
+            latLabel.Text = "Your latitude is: " + lat;
+            lonLabel.Text = "Your longitude is: " + lon;
+
+        }
+    }
+}
